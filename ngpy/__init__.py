69c0a97babaf52b76e17cd37d13b4472b9a03e7a
BigTallahasee <85016240+BigTallahasee@users.noreply.github.com>
2024-07-12 16:00:37 -0500

    Delete ngpy directory

--- ngpy/__init__.py
+++
@@ -1,5 +0,0 @@
-from NGApi import NGApi
-from responses.UserResponse import UserResponse
-from responses.CountryResponse import CountryResponse
-from responses.MarketResponse import MarketResponse
-from responses.NotationResponse import NotationResponse
